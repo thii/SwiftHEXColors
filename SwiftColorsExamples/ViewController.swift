24c92cead044695613388214f67fcb7d11a46e90
Doan Truong Thi <me@thi.codes>
2014-10-04 18:29:11 +0900

    Forgot to type my name :(

--- SwiftColorsExamples/ViewController.swift
+++ SwiftColorsExamples/ViewController.swift
@@ -1,5 +1,7 @@
 // ViewController.swift
 //
+// Copyright (c) 2014 Doan Truong Thi
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
