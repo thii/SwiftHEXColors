dda8034b72e3c9400cf9171c8c836ddcebf4b827
Doan Truong Thi <me@thi.codes>
2014-10-04 18:11:12 +0900

    Initial Commit

---
+++ SwiftColorsExamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftColorsExamples
+//
+//  Created by Thi on 10/4/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
