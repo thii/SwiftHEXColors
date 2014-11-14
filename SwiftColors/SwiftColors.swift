38858b09b7eaf05f5f9b6536ad050bb9b0c405a4
Doan Truong Thi <t@thi.im>
2014-11-14 15:12:18 +0900

    Add support for Max OS X

--- SwiftColors/SwiftColors.swift
+++ SwiftColors/SwiftColors.swift
@@ -20,6 +20,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+// MARK: - UIKit -
+
+#if os(iOS)
+
 import UIKit
 
 extension UIColor {
@@ -99,3 +103,91 @@ extension UIColor {
     self.init(hexString: hexString, alpha: alpha)
   }
 }
+
+#endif
+
+// MARK: - AppKit -
+
+#if os(OSX)
+
+import Cocoa
+
+extension NSColor {
+  /**
+    Create non-autoreleased color with in the given hex string
+    Alpha will be set as 1 by default
+    
+    :param:   hexString
+    :returns: color with the given hex string
+  */
+  convenience init(hexString: String) {
+    self.init(hexString: hexString, alpha: 1.0)
+  }
+  
+  /**
+    Create non-autoreleased color with in the given hex string and alpha
+    
+    :param:   hexString
+    :param:   alpha
+    :returns: color with the given hex string and alpha
+  */
+  convenience init(hexString: String, alpha: Float) {
+    var hex = hexString
+    
+    // Check for hash and remove the hash
+    if hex.hasPrefix("#") {
+      hex = hex.substringFromIndex(advance(hex.startIndex, 1))
+    }
+  
+    // Check for string length
+    assert(countElements(hex) == 6 || countElements(hex) == 3)
+    
+    // Deal with 3 character Hex strings
+    if countElements(hex) == 3 {
+      var redHex   = hex.substringToIndex(advance(hex.startIndex, 1))
+      var greenHex = hex.substringWithRange(Range<String.Index>(start: advance(hex.startIndex, 1), end: advance(hex.startIndex, 2)))
+      var blueHex  = hex.substringFromIndex(advance(hex.startIndex, 2))
+      
+      hex = redHex + redHex + greenHex + greenHex + blueHex + blueHex
+    }
+    
+    let redHex = hex.substringToIndex(advance(hex.startIndex, 2))
+    let greenHex = hex.substringWithRange(Range<String.Index>(start: advance(hex.startIndex, 2), end: advance(hex.startIndex, 4)))
+    let blueHex = hex.substringWithRange(Range<String.Index>(start: advance(hex.startIndex, 4), end: advance(hex.startIndex, 6)))
+    
+    var redInt:   CUnsignedInt = 0
+    var greenInt: CUnsignedInt = 0
+    var blueInt:  CUnsignedInt = 0
+    
+    NSScanner(string: redHex).scanHexInt(&redInt)
+    NSScanner(string: greenHex).scanHexInt(&greenInt)
+    NSScanner(string: blueHex).scanHexInt(&blueInt)
+    
+    self.init(red: CGFloat(redInt) / 255.0, green: CGFloat(greenInt) / 255.0, blue: CGFloat(blueInt) / 255.0, alpha: CGFloat(alpha))
+  }
+  
+  /**
+    Create non-autoreleased color with in the given hex value
+    Alpha will be set as 1 by default
+    
+    :param:   hex
+    :returns: color with the given hex value
+  */
+  convenience init(hex: Int) {
+    self.init(hex: hex, alpha: 1.0)
+  }
+  
+  /**
+    Create non-autoreleased color with in the given hex value and alpha
+    
+    :param:   hex
+    :param:   alpha
+    :returns: color with the given hex value and alpha
+  */
+  convenience init(hex: Int, alpha: Float) {
+    var hexString = NSString(format: "%2X", hex)
+    self.init(hexString: hexString, alpha: alpha)
+  }
+}
+
+#endif
