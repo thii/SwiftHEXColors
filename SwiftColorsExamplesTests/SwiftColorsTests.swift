abacd2f14eee559b5fd07cefc16f6641b386af5a
Evgeniy Dubinin <thisiskilling@me.com>
2014-12-16 01:55:09 +0200

    - Making all initializers to be a failable ones, because creating something from string is failable by definition - Correct Unit Tests to accomodate the change - Replacing assertion w/ regExp validation, so in case we get incorrect forma 'ni' is returned from constructor

--- SwiftColorsExamplesTests/SwiftColorsTests.swift
+++ SwiftColorsExamplesTests/SwiftColorsTests.swift
@@ -29,26 +29,29 @@ class SwiftColorsTests: XCTestCase {
     // is alpha equals to 1 by default after init
     func testAlphaInHexStringInit() {
         let hexBlackColor = SWColor(hexString: "000000")
+        XCTAssertNotNil(hexBlackColor)
         var white: CGFloat = 0.0
         var alpha: CGFloat = 0.0
-        hexBlackColor.getWhite(&white, alpha: &alpha)
+        hexBlackColor!.getWhite(&white, alpha: &alpha)
         let expectedAlpha: CGFloat = 1.0
         XCTAssertEqual(alpha, expectedAlpha)
     }
     
     func testAlphaInHexStringAlphaInit() {
         let hexBlackColor = SWColor(hexString: "000000", alpha: 0.5)
+        XCTAssertNotNil(hexBlackColor)
         var white: CGFloat = 0.0
         var alpha: CGFloat = 0.0
-        hexBlackColor.getWhite(&white, alpha: &alpha)
+        hexBlackColor!.getWhite(&white, alpha: &alpha)
         let expectedAlpha: CGFloat = 0.5
         XCTAssertEqual(alpha, expectedAlpha)
     }
     
     func testBlackColor() {
         let hexBlackColor = SWColor(hexString: "000000")
+        XCTAssertNotNil(hexBlackColor)
         let blackColor = SWColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-        XCTAssertEqual(hexBlackColor, blackColor, "Black color mismatch")
+        XCTAssertEqual(hexBlackColor!, blackColor, "Black color mismatch")
     }
     
     // test how Apple might create the black color
@@ -61,15 +64,18 @@ class SwiftColorsTests: XCTestCase {
     // does # result in correct color
     func testHashColorString() {
         let hexWhiteColor = SWColor(hexString: "#FFFFFF")
+        XCTAssertNotNil(hexWhiteColor)
         let whiteColor = SWColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        XCTAssertEqual(hexWhiteColor, whiteColor)
+        XCTAssertEqual(hexWhiteColor!, whiteColor)
     }
     
     // does string w/ # and w/o # results in same color
     func testWithAndWithoutHash() {
-        let aColor = SWColor(hexString: "#490d87")
-        let aColorToCompare = SWColor(hexString: "490d87")
-        XCTAssertEqual(aColor, aColorToCompare)
+        let colorWithHash = SWColor(hexString: "#490d87")
+        XCTAssertNotNil(colorWithHash)
+        let colorWithoutHash = SWColor(hexString: "490d87")
+        XCTAssertNotNil(colorWithoutHash)
+        XCTAssertEqual(colorWithHash!, colorWithoutHash!)
     }
     
     func testIncorrectFormatString() {
@@ -79,10 +85,11 @@ class SwiftColorsTests: XCTestCase {
         XCTAssertNil(wrongColor02)
         let wrongColor03 = SWColor(hexString: "-FFFFFF")
         XCTAssertNil(wrongColor03)
-        let wrongColor04 = SWColor(hexString: "#FFFFFF")
+        let wrongColor04 = SWColor(hexString: "#FFFFF")
         XCTAssertNil(wrongColor04)
         let wrongColor05 = SWColor(hexString: "#GGG")
         XCTAssertNil(wrongColor05)
-        
+        let wrongColor06 = SWColor(hexString: "HHH")
+        XCTAssertNil(wrongColor06)
     }
 }
