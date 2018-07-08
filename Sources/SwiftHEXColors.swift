b18649fc7efc8471ff23b8674a971ec1a0ff20af
Thi <t@thi.im>
2018-07-08 23:30:01 +0900

    fileprivate to private

--- Sources/SwiftHEXColors.swift
+++ Sources/SwiftHEXColors.swift
@@ -46,14 +46,14 @@ public extension SWColor {
         self.init(hexString: hexString, alpha: 1.0)
     }
 
-    fileprivate convenience init?(hex3: Int, alpha: Float) {
+    private convenience init?(hex3: Int, alpha: Float) {
         self.init(red:   CGFloat( ((hex3 & 0xF00) >> 8).duplicate4bits() ) / 255.0,
                   green: CGFloat( ((hex3 & 0x0F0) >> 4).duplicate4bits() ) / 255.0,
                   blue:  CGFloat( ((hex3 & 0x00F) >> 0).duplicate4bits() ) / 255.0,
                   alpha: CGFloat(alpha))
     }
 
-    fileprivate convenience init?(hex6: Int, alpha: Float) {
+    private convenience init?(hex6: Int, alpha: Float) {
         self.init(red:   CGFloat( (hex6 & 0xFF0000) >> 16 ) / 255.0,
                   green: CGFloat( (hex6 & 0x00FF00) >> 8 ) / 255.0,
                   blue:  CGFloat( (hex6 & 0x0000FF) >> 0 ) / 255.0, alpha: CGFloat(alpha))
